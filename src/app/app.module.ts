a7e54de00e5e191e0d029d38fd682c26c69c66b6
remotestack377 <digamber@positronx.io>
2023-07-09 16:51:18 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    FormsModule,
+    FileUploadModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule {}
